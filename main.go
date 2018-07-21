e410cc66ced3aab4fb109ad31a3f8d39fb41a607
Mike Schinkel <mike@newclarity.net>
2018-07-21 03:11:07 -0400

    Initial Commit

---
+++ main.go
@@ -0,0 +1,83 @@
+/**
+ * See also: https://github.com/tidwall/gjson
+ */
+package main
+
+import (
+	"io/ioutil"
+	"fmt"
+	"os"
+	"github.com/tidwall/gjson"
+	"strings"
+)
+
+type Project struct {
+	json []byte
+	Stack Stack
+}
+
+func NewProject() Project {
+	var project Project
+
+	json, err := ioutil.ReadFile("./project.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	project.json = json
+	return project
+}
+
+type ComponentList []*Component
+
+func (p *Project) GetStackComponents() ComponentList {
+	r:= gjson.GetBytes(p.json,"stack" )
+	rm:= r.Map()
+	cl := make(ComponentList,len(rm))
+	for n,v:= range rm {
+		ct := strings.Split(n,"/")
+		cl[v.Index] = &Component{
+			Index: v.Index,
+			Name:  v.String(),
+			Type: ComponentType{
+				Group: ct[0],
+				Type: ct[1],
+			},
+		}
+	}
+	return cl
+}
+
+type Stack struct {
+	Components ComponentList
+}
+
+type Component struct {
+	Index int
+	Name  string
+	Type  ComponentType
+	Class ComponentClass
+}
+
+type ComponentClass struct {
+	Source string
+	Group string
+	Type string
+	Version string
+}
+
+type ComponentType struct {
+	Source string
+	Group string
+	Type string
+	Version string
+}
+
+func main() {
+	project:= NewProject()
+	for _, c:= range project.GetStackComponents() {
+		t:= c.Type
+		fmt.Printf("\n[%d] %-22v %v",c.Index,t.Group+"/"+t.Type+":",c.Name)
+	}
+
+}
