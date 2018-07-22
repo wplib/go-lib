338127f2dde4e63945149f93cce3cac4cebc7d22
Mike Schinkel <mike@newclarity.net>
2018-07-22 01:33:13 -0400

    Yet-to-be-tested work to evolve components.

--- project-json/components.go
+++ project-json/components.go
@@ -9,46 +9,44 @@ import (
 type ComponentList []*Component
 
 type Component struct {
-	Class ComponentClass
-	Type  *ComponentType
-	Source string
-	Stack string
-	Name string
-	Version string
+	Class     ComponentClass
+	Type      *ComponentType
+	Reference *ComponentReference
 }
 
-func NewStackComponent(n string) *Component {
-	return NewComponent(StackComponent,n)
+func NewStackComponent(t string,r string) *Component {
+	return NewComponent(StackComponent,t,r)
 }
 
-func NewServiceComponent(n string) *Component {
-	return NewComponent(ServiceComponent,n)
+func NewServiceComponent(t string,r string) *Component {
+	return NewComponent(ServiceComponent,t,r)
 }
 
-func NewExecutableComponent(n string) *Component {
-	return NewComponent(ExecutableComponent,n)
+func NewExecutableComponent(t string,r string) *Component {
+	return NewComponent(ExecutableComponent,t,r)
 }
 
-func NewScriptComponent(n string) *Component {
-	return NewComponent(ScriptComponent,n)
+func NewScriptComponent(t string,r string) *Component {
+	return NewComponent(ScriptComponent,t,r)
 }
 
-func NewSourceComponent(n string) *Component {
-	return NewComponent(SourceComponent,n)
+func NewSourceComponent(t string,r string) *Component {
+	return NewComponent(SourceComponent,t,r)
 }
 
-func NewDataComponent(n string) *Component {
-	return NewComponent(DataComponent,n)
+func NewDataComponent(t string,r string) *Component {
+	return NewComponent(DataComponent,t,r)
 }
 
-func NewMediaComponent(n string) *Component {
-	return NewComponent(MediaComponent,n)
+func NewMediaComponent(t string,r string) *Component {
+	return NewComponent(MediaComponent,t,r)
 }
 
-func NewComponent(c ComponentClass, n string) *Component {
+func NewComponent(c ComponentClass,t string,r string) *Component {
 	return &Component{
 		Class: c,
-		Name: n,
+		Type: NewComponentType(t),
+		Reference: NewComponentReference(r),
 	}
 }
 
@@ -65,6 +63,7 @@ const (
 )
 
 type ComponentType struct {
+	Type 	string
 	Host    string
 	Stack   string
 	Role    string
@@ -74,35 +73,68 @@ type ComponentType struct {
 /**
  * @todo Initialize missing from .defaults.componentType
  */
-func NewComponentType(n string) *ComponentType {
-	ct := strings.Split(n,"/")
-	var c *ComponentType
-	switch(len(ct)) {
-	case 1:
-		c= &ComponentType{
-			Host: "wplib.org",
-			Stack: "wordpress",
-			Role:  ct[0],
+func NewComponentType(ts string) *ComponentType {
+	ta := []string{"wplib.org","wordpress","","1"}
+	tp := strings.Split(ts,"/")
+	for i:=2; i>=0; i++ {
+		if (len(tp)<i) {
+			continue
 		}
-	case 2:
-		c= &ComponentType{
-			Host: "wplib.org",
-			Stack: ct[0],
-			Role:  ct[1],
-		}
-	case 3:
-		c= &ComponentType{
-			Host: ct[0],
-			Stack: ct[1],
-			Role:  ct[2],
-		}
+		ta[i] = tp[2-i]
 	}
-	return c
+	tp = strings.Split(ta[2],":")
+	if (len(tp)>=2) {
+		ta[2] = tp[0]
+		ta[3] = tp[1]
+	}
+	vn, err:= strconv.Atoi(ta[3])
+	if err != nil {
+		vn = 1
+	}
+	return &ComponentType{
+		Type:    ts,
+		Host:    ta[0],
+		Stack:   ta[1],
+		Role:    ta[2],
+		Version: byte(vn),
+	}
 }
-func (ct *ComponentType) Name() string {
-	return ct.Stack+"/"+ct.Role
+func (ct *ComponentType) FullType() string {
+	return ct.Host+"/"+ct.Stack+"/"+ct.Role+":"+strconv.Itoa(int(ct.Version))
 }
 
+type ComponentReference struct {
+	Host    string
+	Group   string
+	Name    string
+	Version *ComponentVersion
+}
+
+/**
+ * @todo Initialize missing from .defaults.componentType
+ */
+func NewComponentReference(rs string) *ComponentReference {
+	ra := []string{"","","",""}
+	rp := strings.Split(rs,"/")
+	for i:=2; i>=0; i++ {
+		if (len(rp)<i) {
+			continue
+		}
+		ra[i] = rp[2-i]
+	}
+	rp = strings.Split(ra[2],":")
+	if (len(rp)>=2) {
+		ra[2] = rp[0]
+		ra[3] = rp[1]
+	}
+
+	return &ComponentReference{
+		Host:    ra[0],
+		Group:   ra[1],
+		Name:    ra[2],
+		Version: NewComponentVersion(ra[3]),
+	}
+}
 
 type ComponentVersion struct {
 	Version string
@@ -110,6 +142,10 @@ type ComponentVersion struct {
 	Minor   byte
 	Patch   byte
 }
+
+/**
+ * @todo Initialize missing from .defaults.componentType
+ */
 func NewComponentVersion(sv string) *ComponentVersion {
 	va := []byte{0, 0, 0}
 	vp := strings.Split(sv,".")
@@ -117,7 +153,7 @@ func NewComponentVersion(sv string) *ComponentVersion {
 		if (len(vp)<i) {
 			continue
 		}
-		vn, err:= strconv.Atoi(vp[0])
+		vn, err:= strconv.Atoi(vp[i])
 		if err != nil {
 			break
 		}
