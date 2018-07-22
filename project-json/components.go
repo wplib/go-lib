1a89416db4505986df1ba890147e882370cd3715
Mike Schinkel <mike@newclarity.net>
2018-07-22 00:52:48 -0400

    In-progress work to evolve components.

--- project-json/components.go
+++ project-json/components.go
@@ -1,7 +1,10 @@
 
 package project_json
 
-import "strings"
+import (
+	"strings"
+	"strconv"
+)
 
 type ComponentList []*Component
 
@@ -13,24 +16,31 @@ type Component struct {
 	Name string
 	Version string
 }
+
 func NewStackComponent(n string) *Component {
 	return NewComponent(StackComponent,n)
 }
+
 func NewServiceComponent(n string) *Component {
 	return NewComponent(ServiceComponent,n)
 }
+
 func NewExecutableComponent(n string) *Component {
 	return NewComponent(ExecutableComponent,n)
 }
+
 func NewScriptComponent(n string) *Component {
 	return NewComponent(ScriptComponent,n)
 }
+
 func NewSourceComponent(n string) *Component {
 	return NewComponent(SourceComponent,n)
 }
+
 func NewDataComponent(n string) *Component {
 	return NewComponent(DataComponent,n)
 }
+
 func NewMediaComponent(n string) *Component {
 	return NewComponent(MediaComponent,n)
 }
@@ -55,21 +65,73 @@ const (
 )
 
 type ComponentType struct {
-	Source string
-	Stack string
-	Type string
-	Version string
+	Host    string
+	Stack   string
+	Role    string
+	Version byte
 }
 
+/**
+ * @todo Initialize missing from .defaults.componentType
+ */
 func NewComponentType(n string) *ComponentType {
 	ct := strings.Split(n,"/")
-	return &ComponentType{
-		Stack: ct[0],
-		Type: ct[1],
+	var c *ComponentType
+	switch(len(ct)) {
+	case 1:
+		c= &ComponentType{
+			Host: "wplib.org",
+			Stack: "wordpress",
+			Role:  ct[0],
+		}
+	case 2:
+		c= &ComponentType{
+			Host: "wplib.org",
+			Stack: ct[0],
+			Role:  ct[1],
+		}
+	case 3:
+		c= &ComponentType{
+			Host: ct[0],
+			Stack: ct[1],
+			Role:  ct[2],
+		}
 	}
+	return c
 }
 func (ct *ComponentType) Name() string {
-	return ct.Stack+"/"+ct.Type
+	return ct.Stack+"/"+ct.Role
 }
 
 
+type ComponentVersion struct {
+	Version string
+	Major   byte
+	Minor   byte
+	Patch   byte
+}
+func NewComponentVersion(sv string) *ComponentVersion {
+	va := []byte{0, 0, 0}
+	vp := strings.Split(sv,".")
+	for i:=0; i<=2; i++ {
+		if (len(vp)<i) {
+			continue
+		}
+		vn, err:= strconv.Atoi(vp[0])
+		if err != nil {
+			break
+		}
+		va[i] = byte(vn)
+	}
+	return &ComponentVersion{
+		Version: sv,
+		Major:   va[0],
+		Minor:   va[1],
+		Patch:   va[2],
+	}
+}
+func (cv *ComponentVersion) FullVersion() string {
+	return strconv.Itoa(int(cv.Major))+"."+
+		strconv.Itoa(int(cv.Minor))+"."+
+		strconv.Itoa(int(cv.Patch))
+}
